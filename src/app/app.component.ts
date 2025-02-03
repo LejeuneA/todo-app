cbbf9de7828ebe1e17c07d5db8292fe27fa6cac9
Acelya Lejeune <acelyalejeune@gmail.com>
2025-02-03 22:04:38 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    TodoListComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'todo-app';
+  public list: string[] = ['TODO 1', 'TODO 2', 'TODO 3', 'TODO 4', 'TODO 5'];
+
+  public fc = new FormControl();
+
+  public deleteItemInList(itemName: string): void {
+    this.list = this.list.filter((item) => item !== itemName);
+  }
+
+  public addItem(): void {
+    this.list.push(this.fc.value);
+    this.fc.reset();
+  }
 }

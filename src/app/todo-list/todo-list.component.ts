ea045165d86ccf42eafe8f92b8fff0e7283430b4
Acelya Lejeune <acelyalejeune@gmail.com>
2025-02-03 22:04:41 +0100

    Update todo-list.component.ts

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [MatListModule, MatIconModule, MatIconButton],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.scss',
+})
+export class TodoListComponent {
+  @Input() public todolist: string = [];
+
+  @Output() public onDeleteItem = new EventEmitter<string>();
+
+  public deleteItem(itemName: string): void {
+    this.deleteItem.emit(itemName);
+  }
+}

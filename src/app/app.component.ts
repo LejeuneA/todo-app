dee2101a8bf9faec40acbfd576f7f8f6e440beb1
Acelya Lejeune <acelyalejeune@gmail.com>
2025-02-05 19:10:07 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
+// import { HeaderComponent } from './header/header.component';
 import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   imports: [
-    HeaderComponent,
+    // HeaderComponent,
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,

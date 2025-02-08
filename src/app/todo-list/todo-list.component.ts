330ee880baa9514f46a1226abf90076b9e7f0888
A.Lejeune <acelyalejeune@gmail.com>
2025-02-08 17:12:09 +0100

    Update todo-list.component.ts

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -2,11 +2,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms'; // ✅ Import FormsModule
+import { CommonModule } from '@angular/common'; // ✅ Import CommonModule
 import { ITodo } from '../interfaces/todo.interface';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [MatListModule, MatIconModule, MatButtonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+  ], // ✅ Add CommonModule
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
 })
@@ -15,12 +24,19 @@ export class TodoListComponent {
 
   @Output() public onDeleteTodo = new EventEmitter<string>();
   @Output() public onTodoDone = new EventEmitter<string>();
+  @Output() public onUpdateTodo = new EventEmitter<ITodo>();
 
-  public deleteTodo(todo: ITodo): void {
-    this.onDeleteTodo.emit(todo.id);
+  public editTodo(todo: ITodo): void {
+    todo.isEditing = true;
   }
 
-  public doneTodo(todo: ITodo): void {
-    this.onTodoDone.emit(todo.id);
+  public saveEdit(todo: ITodo): void {
+    todo.isEditing = false;
+    this.onUpdateTodo.emit(todo);
+  }
+
+  public toggleDone(todo: ITodo): void {
+    todo.isDone = !todo.isDone; // Toggle status
+    this.onTodoDone.emit(todo.id); // Emit the event with the todo ID
   }
 }
